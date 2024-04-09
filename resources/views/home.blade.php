200853654dfde49609ffdfaf864ba3b3bed2f629
federico-dibitonto <f.dibitonto94@gmail.com>
2024-04-09 14:29:42 +0200

    exercise completed with bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,28 @@
 
 
 @section('content')
-    <div class="container-xl py-5">
-        <h1>Il mio film preferito:</h1>
+    <div class="container-xl py-5 text-center">
+        <h1 class="pb-5 text-danger">MY MOVIE DATABASE</h1>
 
-        @foreach($movies as $movie)
-            {{$movie->title}}<br>
-        @endforeach
+        <div class="d-flex flex-wrap my_grid">
+            @foreach($movies as $movie)
+                <div class="my_card card rounded-4 overflow-hidden">
+                    <img src="{{ $movie->image }}" alt="">
+                    <div class="p-4">
+                        <h3 class="text-danger align-self-center">
+                            {{$movie->title}}
+                        </h3> 
+                        <div>
+                            <span class="text-danger">Titolo Originale</span>
+                            {{$movie->original_title}}
+                        </div>
+                        <div>
+                            <span class="text-danger">Voto</span>
+                            {{$movie->vote}}
+                        </div>   
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
